8e83dd7340841336478709c5d7d99d6cade2b8b0
apal <alishapal96@gmail.com>
2021-06-30 21:11:46 -0700

    add local storage functions

--- Develop/script.js
+++ Develop/script.js
@@ -5,3 +5,25 @@ $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));//format day, Mont
 
 
 })
+var con = document.querySelector(".container");
+var textBoxInput = document.querySelector(".description");
+var saveButton = document.querySelector(".saveBtn");
+
+console.log(con);
+function storeText(){
+    var text = textBoxInput.value.trim();
+    localStorage.setItem("text",JSON.stringify(text));
+};
+function renderText(){
+    var storedText = JSON.parse(localStorage.getItem("text"))
+    if (storedText !== null) {
+        text = storedText;
+      }
+
+};
+saveButton.addEventListener("click",function(event){
+    event.preventDefault();
+    
+    
+    
+});
