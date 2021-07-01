9b3a1b08e7f8ae7684357c76bf28fb3754fec1d7
apal <alishapal96@gmail.com>
2021-06-30 20:09:08 -0700

    add date and time

---
+++ Develop/script.js
@@ -0,0 +1,7 @@
+//Will run when the page is loaded.
+$(document).ready(function () {
+
+$("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));//format day, Month  date , year EX:Wednesday, June 30th, 2021
+
+
+})
